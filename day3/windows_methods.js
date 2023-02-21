1ebfd411d0442e147b70fa35afe7442e9a32c03b
Camilo Saavedra <ci.saavedras@gmail.com>
2023-02-21 10:17:11 -0300

    Add example windows method

---
+++ day3/windows_methods.js
@@ -0,0 +1,10 @@
+//Alert: alert(message)
+alert('Welcome to 30DaysOfJavaScript');
+
+//Prompt: prompt('required text', 'optional text')
+let number = prompt('Enter number', 'number goes here');
+console.log(number);
+
+//Confirm
+const agree = confirm('Are you sure you like to delete? ')
+console.log(agree) // result will be true or false based on what you click on the dialog box
